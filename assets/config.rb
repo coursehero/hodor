b52eefc7acefea9a1011dd9ca393b2b941684a5f
Paddu <haripaddu@gmail.com>
2016-07-12 17:22:41 -0700

    Setting folder structure for Hodor

--- assets/config.rb
+++ assets/config.rb
@@ -4,12 +4,12 @@ require 'compass/import-once/activate'
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "css"
-sass_dir = "scss"
+sass_dir = "sass"
 images_dir = "img"
 javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
+output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
