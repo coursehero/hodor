484a0323e5f4068808717ccb67ee0bc2ce53f922
Paddu Raghavan <haripaddu@gmail.com>
2016-07-15 01:10:00 -0700

    Setting Grid Framework

--- assets/config.rb
+++ assets/config.rb
@@ -9,7 +9,7 @@ images_dir = "img"
 javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
-output_style = :compressed
+output_style = :expanded
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
@@ -23,3 +23,6 @@ output_style = :compressed
 # preferred_syntax = :sass
 # and then run:
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+
+# Extensions
+require 'breakpoint'
